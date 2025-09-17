3b930af9720dce4602139474526fe6952d15f1b7
1212445 <152827567+1212445@users.noreply.github.com>
2025-09-17 19:22:06 +0800

    feat(pc-admin): add App.vue root component

---
+++ pc-admin/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <router-view />
+</template>
+
+<style>
+html, body, #app { height: 100%; }
+body { margin: 0; }
+</style>
